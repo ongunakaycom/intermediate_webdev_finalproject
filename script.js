fc753ffe3e0c3ff4b59eb6ae037c44611dc788b9
Ongun <info@ongunakay.com>
2025-03-23 15:16:08 +0300

    commit

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-function compute()
+function compute() 
 {
-		//Create variables from input
-    let principal = document.getElementById("principal").value;
-    let rate = document.getElementById("rate").value;
-    let years = document.getElementById("years").value;
-    
-    //do math
-    let interest = principal * years * rate / 100;
-    let endYear = new Date().getFullYear() + parseInt(years);
+    // Convert inputs to numbers
+    let principal = Number(document.getElementById("principal").value);
+    let rate = Number(document.getElementById("rate").value);
+    let years = Number(document.getElementById("years").value);
 
-    //compound interest formula
-    let compounded = principal * ((1 + ((rate/100)/12)) ** (12*years))
-		
-    //validate the principal. If negative or 0 alert user and focus on principal
-    if(principal <= 0){
+    // Validate the principal. If negative or 0, alert user and focus on principal
+    if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
-    
-    //display result
-    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br>at an interest rate of 
-    <mark>${rate}</mark>.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>
+
+    // Perform calculations
+    let interest = principal * years * rate / 100;
+    let endYear = new Date().getFullYear() + years;
+
+    // Compound interest formula
+    let compounded = principal * ((1 + ((rate / 100) / 12)) ** (12 * years));
+
+    // Display result
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal.toFixed(2)}</mark>,<br>at an interest rate of 
+    <mark>${rate.toFixed(2)}</mark>%.<br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br>in the year <mark>${endYear}</mark><br>
     <br>With monthly compounded interest your balance would instead be <mark>${compounded.toFixed(2)}</mark>`;
 }
 
