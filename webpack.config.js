7796dd562bbd1af20ad71bfb5e5c1f61e9efe08f
Ongun <info@ongunakay.com>
2025-03-24 00:44:07 +0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,20 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/script.js', // Main JS entry point
+  entry: './src/main.js', // Main JS entry point
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',  // Ensure this points to the correct root
+    publicPath: '/',  // Ensure this points to the correct root for deployment
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Source HTML file
+      template: './src/index.html', // Source HTML template file
+      favicon: './src/assets/favicon.ico'  // Automatically inject favicon
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'src/assets', to: 'assets' },  // Copy assets like favicon
+        { from: 'src/assets', to: 'assets' },  // Copy assets (like favicon) to the dist folder
       ],
     }),
     new MiniCssExtractPlugin({
